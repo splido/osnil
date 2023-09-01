f58d32eac99620bbd96ebc70c73d0aa710c6de04
PawanRajput2000 <110086934+PawanRajput2000@users.noreply.github.com>
2023-09-01 17:51:16 +0530

    Update CommentPopup.jsx

--- src/components/CommentPopup.jsx
+++ src/components/CommentPopup.jsx
@@ -1,78 +1,72 @@
-import { useState } from "react"
-import CommentList from "./CommentList"
-function CommentPopup({info, id}) {
-  const [comment, setComment] = useState('')
-  const comments = info.subscription.comment
-  const applicationId = info._id
-  const authToken = id
-  console.log(id)
-  const apiUrl = `https://appsalabackend-p20y.onrender.com/comment/${applicationId}`;  
+import React, { useState, useEffect } from "react";
+import CommentList from "./CommentList";
+
+// Function to sort comments by date in descending order
+const sortCommentsByDate = (comments) => {
+  return comments.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+};
+
+function CommentPopup({ info }) {
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState(sortCommentsByDate(info.subscription.comment));
+
+  const authToken = localStorage.getItem("access_token");
+  const apiUrl = `https://appsalabackend-p20y.onrender.com/comment/${info.obj_id._id}`;
 
   const handleChange = (e) => {
-    setComment(e.target.value)
+    setComment(e.target.value);
+  };
 
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  const commentObj={
-    comment: comment,
-  }
-    // Data to be sent in the POST request
-  const postData = {
-    comments: [...comments, commentObj],
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ comment: comment }),
+    };
+
+    try {
+      const response = await fetch(apiUrl, requestOptions);
+      const data = await response.json();
+      console.log("Response data:", data);
+
+      if (data.status === true) { // Add the new comment to the top of the comments list with the current date
+        const newComment = { ...data, createdAt: new Date().toISOString() };
+        setComments((prevComments) => [newComment, ...prevComments]);
+      }else{
+        alert(data.message)
+      }
+
+      setComment("");
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
-  // Configure the request
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-    "Authorization": `Bearer ${authToken}`,
-    "Content-Type": "application/json"
-      // You can add other headers here if needed
-    },
-    body: JSON.stringify(postData),
-  };
-  
-  // Perform the fetch request
-  fetch(apiUrl, requestOptions)
-    .then(response => {
-      response.json()})
-    .then(data => {
-      console.log('Response data:', data);
-      // Handle the response data here
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      // Handle errors here
-    });
-  }
+  console.warn(comment, comments)
   return (
-    <>
     <div className="comment-popup">
-      <div className="comment-heading">
-        <h3>My Comments</h3>  
-      <p>Tell us about your experience</p>
-    </div>
-    <div class="line"></div>
-      <form action="" className="comment-form" onChange={handleChange} onSubmit={handleSubmit}>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button className="button">Comment</button>
+      <form onSubmit={handleSubmit} className="comment-form">
+        <textarea
+          value={comment}
+          onChange={handleChange}
+          placeholder="Write your comment..."
+          rows="4"
+        />
+        <button type="submit">Comment</button>
       </form>
-      <div className="comments-section">
-      <h3 className="comment-heading">Previous Comments</h3>
-   
-  
-       
-        {
- comments?.map((comment)=>(   
-  <CommentList comment={comment}/>
-))
-        }
-        
-    </div>
-      </div>
 
-    </>
-  )
+      <div className="comments-section">
+        <h3 className="comment-heading">Previous Comments</h3>
+        {comments.map((comment, index) => (
+          <CommentList key={comment._id} comment={comment} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default CommentPopup
+export default CommentPopup;
